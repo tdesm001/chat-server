2d8b314d0868f85da8c417afe8d727d0a058651f
kraghack <kraghack@fea.st>
2015-05-11 18:48:42 -0500

    Parse number configs

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var request = require('request');
 var _ = require('lodash');
 
 var config = {
-  port:         process.env.PORT || 3001,
+  port:         (process.env.PORT && parseInt(process.env.PORT)) || 3001,
   api_endpoint: process.env.API_ENDPOINT || 'https://api.moneypot.com/v1',
-  app_id:       process.env.APP_ID || 1007,
+  app_id:       (process.env.APP_ID && parseInt(process.env.APP_ID)) || 1007,
   app_secret:   process.env.APP_SECRET || '3fc58b9e-3426-4705-9ad8-85ca32603d4b'
 };
 
