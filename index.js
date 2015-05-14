141f1c3b29d9135123b5afa27414e787bfe9e9e2
kraghack <kraghack@fea.st>
2015-05-14 17:18:48 -0500

    Default api endpoint to localhost

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var CBuffer = require('CBuffer');
 
 var config = {
   port:         (process.env.PORT && parseInt(process.env.PORT)) || 3001,
-  api_endpoint: process.env.API_ENDPOINT || 'https://api.moneypot.com/v1',
+  api_endpoint: process.env.API_ENDPOINT || 'http://localhost:3000/v1',
   app_id:       (process.env.APP_ID && parseInt(process.env.APP_ID)) || 1007,
   app_secret:   process.env.APP_SECRET || '3fc58b9e-3426-4705-9ad8-85ca32603d4b'
 };
